58aeba85a0bfcc77b882563f3b3fe1c98f26f785
“eightfy” <l44454545@gmail.com>
2022-11-13 21:21:57 +0800

    init

--- demo/main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-
-#appname = main
-
-urlpatterns = [
-    path('', views.mainview, name = 'main'),
-    path('add/', views.addview, name = 'add')
-]
--- demo/main/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-#主界面
-def mainview(request):
-    return render(request, 'main.html')
-#添加新订单
-def addview(request):
-    return render(request, 'add.html')
